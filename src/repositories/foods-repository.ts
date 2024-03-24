b9f2e1b11f6b488629c74991c069b59e61d7499b
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-03-24 18:58:57 -0300

    :sparkles: feat: create repositories

---
+++ src/repositories/foods-repository.ts
@@ -0,0 +1,9 @@
+import type { Food, Prisma } from '@prisma/client'
+
+export interface FoodsRepository {
+  create(data: Prisma.FoodUncheckedCreateInput): Promise<Food>
+  findById(id: string): Promise<Food | null>
+  findMany(page: number, userId: string): Promise<Food[]>
+  updateById(id: string, data: Prisma.FoodUpdateInput): Promise<Food>
+  deleteById(id: string): Promise<void>
+}
---
+++ src/repositories/prisma/prisma-foods-repository.ts
@@ -0,0 +1,46 @@
+import type { Prisma } from '@prisma/client'
+import { prisma } from '../../libs/prisma'
+import type { FoodsRepository } from '../foods-repository'
+
+export class PrismaFoodsRepository implements FoodsRepository {
+  async create(data: Prisma.FoodUncheckedCreateInput) {
+    const user = await prisma.food.create({ data })
+
+    return user
+  }
+
+  async findById(id: string) {
+    const food = await prisma.food.findUnique({
+      where: { id },
+    })
+
+    return food
+  }
+
+  async findMany(page: number, userId: string) {
+    const foods = await prisma.food.findMany({
+      where: {
+        userId,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return foods
+  }
+
+  async updateById(id: string, data: Prisma.FoodUpdateInput) {
+    const food = await prisma.food.update({
+      where: { id },
+      data,
+    })
+
+    return food
+  }
+
+  async deleteById(id: string) {
+    await prisma.food.delete({
+      where: { id },
+    })
+  }
+}
